a57cb09b85159743deabe15aa5d420a76a8c7296
kugue99A <hisabisa99yohey@gmail.com>
2024-09-07 23:05:25 +0900

    add go formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,11 +6,9 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
 				rust = { "rustfmt", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
+				go = { "gopls", lsp_fallback = "fallback" },
 				javascript = { "prettier", "eslint", stop_after_first = true },
 				typescript = { "prettier", "eslint", stop_after_first = true },
 				typescriptreact = { "prettier", "eslint", stop_after_first = true },
@@ -19,7 +17,6 @@ return {
 			format_on_save = {
 				timeout_ms = 2000,
 				lsp_fallback = true,
-				quiet = false,
 			},
 		})
 	end,
